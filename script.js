581f5d67eb2882a15ff7841cd400f4b119004765
hhhrrrttt222111 <43471295+hhhrrrttt222111@users.noreply.github.com>
2020-03-06 17:06:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-var food = document.getElementById('signup');
-
-window.onclick = function(event){
-    if(event.target == food){
-        this.food.style.direction = 'none';
-    }
-}
-
-
-var food1 = document.getElementById('login');
-
-
-window.onclick = function(event) {
-    if (event.target == food1) {
-        modal.style.display = "none";
-    }
-}
 
